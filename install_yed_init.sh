9edf5c51a976aed0fb09cffc76c471ecf5f85538
Brandon Kammerdiener <kammerdienerb@gmail.com>
2020-01-27 16:27:34 -0500

    yedrc

--- install_yed_init.sh
+++ install_yed_init.sh
@@ -4,3 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "Compiling kammerdienerb_init and installing."
 gcc -shared -fPIC ${DIR}/kammerdienerb_init.c -lyed -o ~/.yed/init.so
+cp ${DIR}/yedrc ~/.yed/yedrc
--- kammerdienerb_init.c
+++ kammerdienerb_init.c
@@ -1,96 +1,29 @@
 #include <yed/plugin.h>
 #include <yed/menu_frame.h>
 
-void get_env_info(void);
 int has(char *prg);
 
 int yed_plugin_boot(yed_plugin *self) {
-    int   i, n_plugins;
-    char *plugins[] = {
-/*         "meta_keys", */
-/*         "wait_keys", */
-        "vimish",
-        "brace_hl", "tag_hl", "cursor_word_hl",
-        "lang/c", "lang/sh", "lang/bjou", "lang/latex", "lang/python",
-        "indent_c", "comment",
-        "autotrim", "completer",
-        "grep", "find_file", "man",
-        "style_picker",
-        "focus_frame",
-        "styles/lab", "styles/book", "styles/blue",
-        "styles/first", "styles/elise", "styles/nord", "styles/monokai", "styles/gruvbox",
-        "styles/skyfall", "styles/papercolor",
-        "styles/casey", "styles/cadet",
-    };
+    YEXE("plugin-load", "yedrc");
 
-
-    /* Set variables before loading plugins. */
-    get_env_info();
-
+    /*
+     * Set things that need to be dynamic,
+     * but allow yedrc to override.
+     */
     if (yed_term_says_it_supports_truecolor()) {
         yed_set_var("truecolor", "yes");
     }
-    yed_set_var("tab-width",             "4");
-    yed_set_var("latex-comp-prg",        "xelatex -halt-on-error --interaction=nonstopmode '%'");
-    yed_set_var("latex-view-prg",        "echo \"mupdf '%'\" | $SHELL &");
-    yed_set_var("latex-update-view-prg", "pkill -HUP mupdf");
     if (has("rg")) {
-        yed_set_var("grep-prg",          "rg --vimgrep \"%\"");
+        yed_set_var("grep-prg",      "rg --vimgrep \"%\"");
     }
     if (has("fzf")) {
         yed_set_var("find-file-prg", "fzf --filter=\"%\"");
     }
 
-    /* I like the cursor line. But it's not default. */
-    yed_set_var("cursor-line", "yes");
-    yed_set_var("vimish-insert-no-cursor-line", "yes");
-
-    /* Load all plugins. */
-    YEXE("plugins-add-dir", "./.yed");
-
-    n_plugins = sizeof(plugins) / sizeof(char*);
-
-    for (i = 0; i < n_plugins; i += 1) {
-        YEXE("plugin-load", plugins[i]);
-    }
-
-
-    /* Keybindings */
-    YEXE("vimish-bind", "insert",    "j", "j",                "CMD",    "vimish-exit-insert");
-    YEXE("vimish-bind", "normal",    "spc", "c", "o",         "CMD",    "comment-toggle");
-    YEXE("vimish-bind", "normal",    "spc", "l", "c",         "CMD",    "latex-compile-current-file");
-    YEXE("vimish-bind", "normal",    "spc", "l", "v",         "CMD",    "latex-view-current-file");
-    YEXE("vimish-bind", "normal",    "spc", "r", "d",         "CMD",    "redraw");
-    YEXE("vimish-bind", "normal",    "spc", "v", "s", "p",    "CMD",    "frame-vsplit");
-    YEXE("vimish-bind", "normal",    "spc", "h", "s", "p",    "CMD",    "frame-hsplit");
-    YEXE("vimish-bind", "normal",    "spc", "b", "o",         "CMD",    "fill-command-prompt", "buffer");
-    YEXE("vimish-bind", "normal",    "spc", "b", "d",         "CMD",    "buffer-delete");
-    YEXE("vimish-bind", "normal",    "ctrl-n",                "CMD",    "buffer-next");
-    YEXE("vimish-bind", "normal",    "ctrl-p",                "CMD",    "buffer-prev");
-    YEXE("vimish-bind", "normal",    "M", "M",                "CMD",    "man-word");
-    YEXE("vimish-bind", "normal",    "L", "L",                "CMD",    "fill-command-prompt", "cursor-line");
-    YEXE("vimish-bind", "normal",    "ctrl-l",                "CMD",    "frame-next");
-    YEXE("vimish-bind", "normal",    "ctrl-h",                "CMD",    "frame-prev");
-    YEXE("vimish-bind", "normal",    ">",                     "CMD",    "indent");
-    YEXE("vimish-bind", "normal",    "<",                     "CMD",    "unindent");
-    YEXE("vimish-bind", "normal",    "spc", "g",              "CMD",    "grep");
-    YEXE("vimish-bind", "normal",    "spc", "f",              "CMD",    "find-file");
-
-    /* Colors */
-    YEXE("style", "nord_transparent");
-
-    /* Load a directory-specific plugin if available. */
-    YEXE("plugin-load", "proj");
+    /* Load my yedrc file. */
+    YEXE("yedrc-load", "~/.yed/yedrc");
 
     return 0;
-}
-
-void get_env_info(void) {
-    char *term,
-         *user;
-
-    if ((term = getenv("TERM"))) { yed_set_var("term", term); }
-    if ((user = getenv("USER"))) { yed_set_var("user", user); }
 }
 
 /*
