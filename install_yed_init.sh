ad4beb2e303bdf9d28027735c32ffb4b213551ce
Brandon Kammerdiener <kammerdienerb@gmail.com>
2020-02-28 07:20:40 -0700

    make sure directory exists

--- install_yed_init.sh
+++ install_yed_init.sh
@@ -2,6 +2,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+mkdir -p ~/.yed
+
 echo "Compiling kammerdienerb_init and installing."
 gcc -shared -fPIC ${DIR}/kammerdienerb_init.c -lyed -o ~/.yed/init.so
 cp ${DIR}/yedrc ~/.yed/yedrc
