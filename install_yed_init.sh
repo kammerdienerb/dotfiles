068d5f02869bc6e53e3a0fc28da2be9e8f4ed94f
Brandon Kammerdiener <kammerdienerb@gmail.com>
2020-05-08 10:07:18 -0400

    yed stuff

--- install_yed_init.sh
+++ install_yed_init.sh
@@ -5,6 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir -p ~/.yed
 
 echo "Compiling kammerdienerb_init and installing."
-gcc -shared -fPIC -g ${DIR}/kammerdienerb_init.c -lyed -o ~/.yed/init.so
-gcc -shared -fPIC -g ${DIR}/slide.c -lyed -o ~/.yed/slide.so
+gcc -shared -fPIC -g -O3 ${DIR}/kammerdienerb_init.c -lyed -o ~/.yed/init.so
+gcc -shared -fPIC -g -O3 ${DIR}/slide.c -lyed -o ~/.yed/slide.so
 cp ${DIR}/yedrc ~/.yed/yedrc
--- kammerdienerb_init.c
+++ kammerdienerb_init.c
@@ -21,11 +21,11 @@ int yed_plugin_boot(yed_plugin *self) {
         yed_log("init.c: terminal says it supports truecolor");
         YEXE("set", "truecolor", "yes");
     }
-    if (has("rg")) {
+    if (file_exists_in_PATH("rg")) {
         yed_log("init.c: found an rg executable");
         YEXE("set", "grep-prg", "rg --vimgrep \"%\"");
     }
-    if (has("fzf")) {
+    if (file_exists_in_PATH("fzf")) {
         yed_log("init.c: found a fzf executable");
         YEXE("set", "find-file-prg", "fzf --filter=\"%\"");
     }
@@ -34,25 +34,5 @@ int yed_plugin_boot(yed_plugin *self) {
     YEXE("yedrc-load", "~/.yed/yedrc");
 
     LOG_EXIT();
-    return 0;
-}
-
-/*
- * @bad @performance
- * Amazingly, this is actually the bottleneck for startup time.
- * We're pretty fast about everything else.
- * But it would be nice to have a faster method to get this
- * information.
- */
-int has(char *prg) {
-    char *path;
-
-    path = exe_path(prg);
-
-    if (path) {
-        free(path);
-        return 1;
-    }
-
     return 0;
 }
