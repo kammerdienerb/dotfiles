ff987706223abfbecb9d1b2e70bc161fe2540c7d
Brandon Kammerdiener <kammerdienerb@gmail.com>
2020-05-31 17:37:40 -0400

    more verbose install script

--- install_yed_init.sh
+++ install_yed_init.sh
@@ -6,5 +6,7 @@ mkdir -p ~/.yed
 
 echo "Compiling kammerdienerb_init and installing."
 gcc -shared -fPIC -g -O3 ${DIR}/kammerdienerb_init.c -lyed -o ~/.yed/init.so
+echo "Compiling slide plugin and installing."
 gcc -shared -fPIC -g -O3 ${DIR}/slide.c -lyed -o ~/.yed/slide.so
+echo "Moving yedrc."
 cp ${DIR}/yedrc ~/.yed/yedrc
