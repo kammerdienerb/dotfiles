db3177402e16565648a9d85b0bfe57e4b0ccfeaf
Brandon Kammerdiener <kammerdienerb@gmail.com>
2020-10-30 16:27:31 -0400

    more organized yed setup; added my own language plugins that used to be part of yed

--- install.sh
+++ install.sh
@@ -1,16 +1,32 @@
 #! /usr/bin/env bash
 
-set -x
-
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd $DIR
 
 eval HM=$(echo ~$(logname))
 
+echo ".vimrc"
 cp .vimrc $HM/.vimrc
+echo ".vim/"
 cp -r .vim $HM
-
+echo ".tmux.conf"
 cp .tmux.conf $HM
 
-./install_yed_init.sh
+mkdir -p ~/.yed
+
+YED_DIR=${DIR}/.yed
+HOME_YED_DIR=${HM}/.yed
+for f in $(find ${DIR}/.yed -name "*.c"); do
+    echo "Compiling ${f/${YED_DIR}/.yed} and installing."
+    PLUG_DIR=$(dirname ${f/${YED_DIR}/${HOME_YED_DIR}})
+    PLUG_FULL_PATH=${PLUG_DIR}/$(basename $f ".c").so
+
+    mkdir -p ${PLUG_DIR}
+    gcc -shared -fPIC -g -O3 ${f} -lyed -o ${PLUG_FULL_PATH} &
+done
+
+wait
+
+echo "Moving yedrc."
+cp ${YED_DIR}/yedrc ${HOME_YED_DIR}
--- root_init.c
+++
@@ -1,7 +0,0 @@
-#include <yed/plugin.h>
-
-int yed_plugin_boot(yed_plugin *self) {
-    YEXE("plugin-load", "yedrc");
-    YEXE("yedrc-load", "~/.yed/yedrc");
-    return 0;
-}
