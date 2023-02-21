a0e3f62c9112a737f426e6b2b9e60e407d628761
Brandon Kammerdiener <kammerdienerb@MacBook-Pro-230.local>
2023-02-21 10:13:56 -0500

    simon; style

--- yed/lang/syntax/simon.c
+++ yed/lang/syntax/simon.c
@@ -154,7 +154,7 @@ int yed_plugin_boot(yed_plugin *self) {
         APOP();
 
         APUSH("&code-preprocessor");
-            RANGE("\\$\\("); ONELINE(); ENDRANGE("\\)");
+            REGEX("[a-zA-Z_][0-9a-zA-Z_]*!");
             REGEX("\\\\[a-zA-Z_][0-9a-zA-Z_]*");
         APOP();
 
